370f2a7090a3643357ad2b56384aa40e1e9f3712
Rory Byrne <roryofbyrne@gmail.com>
2017-10-11 11:20:42 +0000

    [Code] Added comments to weighted_choice() and to the seed word

--- src/generator.py
+++ src/generator.py
@@ -9,6 +9,16 @@ def predict_word(probs, word):
     return ("unknown", 0.0)
 
 def weighted_choice(dict):
+    """
+    dict is of the form { word : probability,
+                            word : probability,
+                            ...,
+                            word : probability }
+    We randomly pick a word, but the randomness depends on the probabilities
+
+    :param dict:
+    :return:
+    """
     r = random.uniform(0, 1) # 1 is the total of our probability distribution
     tmp = 0.0
 
@@ -17,8 +27,11 @@ def weighted_choice(dict):
         if r < tmp:
             return k
 
-probs = utils.get_n_gram_probs("/home/rory/projects/ronald/data/trump-tweets.csv", n=3)
+probs = utils.get_n_gram_probs("/home/rory/projects/ronald/data/trump-tweets.csv", n=5)
 
+# this is basically the seed word, to begin the sentence with.
+# will need to figure out a way for the system to choose this word intelligently
+# https://github.com/RoryOfByrne/ronald-trump/issues/4
 next_word = "we"
 x = 0
 while x < 30:
