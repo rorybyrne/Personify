e1f83ede9fc78c5a10793e4456e29695e1df78f7
Rory Byrne <roryofbyrne@gmail.com>
2017-10-11 18:01:06 +0000

    [Code - Gen] Made a 'Generator' class and added multiple (2-n) n-gram functionality for backoff

--- src/generator.py
+++ src/generator.py
@@ -1,31 +1,61 @@
 import random
 import utils
 
-def predict_word(probs, word):
-    if word in probs:
-        choice = weighted_choice(probs[word])
-        return (choice, probs[word][choice])
-
-    return ("unknown", 0.0)
-
-def weighted_choice(dict):
+class Generator:
     """
-    dict is of the form { word : probability,
-                            word : probability,
-                            ...,
-                            word : probability }
-    We randomly pick a word, but the randomness depends on the probabilities
-
-    :param dict:
-    :return:
+    This class will hold all of our models (there's a separate model
+     per 2,3,n-gram of words, sentence length, and POS tag etc.)
+    It contains functions which use those models to generate text.
+    All models are created when the class is initialized with a filename.
+    The entrypoint to get the next word is currently predict_next()
     """
-    r = random.uniform(0, 1) # 1 is the total of our probability distribution
-    tmp = 0.0
+    def __init__(self, sourcefile, word_ngrams=2):
+        self.sourcefile = sourcefile
+        self.word_ngrams = word_ngrams
+        # we store a unique n-gram model for each n=2 -> n
+        self.word_ngram_models = self.build_word_model(word_ngrams, [])
 
-    for k in dict:
-        tmp += dict[k]
-        if r < tmp:
-            return k
+    def build_word_model(self, models):
+        """
+        recursively build a list of models for n > 2
+
+        :param models:
+        :return a list of n-gram models:
+        """
+        n = self.word_ngrams
+        if n < 2:
+            # todo: check that models is not empty
+            return models
+        else:
+            m = utils.get_n_gram_probs(self.sourcefile, n)
+            return [m] + self.build_word_model(n-1)
+
+    def predict_next(self, probs, current):
+        n = self.word_ngrams
+        if current in self.word_ngram_models[n-1]:
+            choice = self._weighted_choice(probs[current])
+            return choice
+        return "UNK"
+
+
+    def _weighted_choice(self, dict):
+        """
+            dict is of the form { event : probability,
+                                    event : probability,
+                                    ...,
+                                    event : probability }
+            We randomly pick an event, but the randomness depends on the probabilities
+
+            :param dict:
+            :return:
+            """
+        r = random.uniform(0, 1)  # 1 is the total of our probability distribution
+        tmp = 0.0
+
+        for k in dict:
+            tmp += dict[k]
+            if r < tmp:
+                return k
 
 n = 3
 probs = utils.get_n_gram_probs("/home/rory/projects/ronald/data/trump-tweets.csv", n)
