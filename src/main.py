c881317f148cb97ec6421cb88feba16fc7ff3c19
Rory Byrne <roryofbyrne@gmail.com>
2017-10-12 12:06:42 +0000

    [Code - Bot] I lied. Now it's functional

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from ronald_brain import generator
+from ronald_mouth import bot
+from sys import exit
+
+filename = "/home/rory/projects/ronald/data/trump-tweets.csv"
+n = 3
+gen = generator.Generator(filename, n)
+
+send_to_twitter = ''
+tweet = ''
+
+while send_to_twitter != "y":
+    if send_to_twitter == "q":
+        exit(0)
+
+    tweet = gen.generate(20)
+
+    print(tweet)
+    send_to_twitter = input("Post to Twitter? y/n\nTo quit, type q\n")
+
+bot.send_tweet(tweet)
+print("Sent!")
