8432aa13895f867dfbcd86a078653be9f968dbe2
RoryOfByrne <roryofbyrne@gmail.com>
2017-10-15 21:09:33 +0000

    Update bot.py

--- src/ronald_mouth/bot.py
+++ src/ronald_mouth/bot.py
@@ -2,8 +2,8 @@ import tweepy
 from ronald_brain import generator
 #import twitter_credentials
 
-auth = tweepy.OAuthHandler('b9nh8obvDFh7YYIKpHrLJ9a2T', '37lhcaIiGie8bA9h1bS0ajxs0eYfB7NFJ3hfUltWhVR9MJTpRt')
-auth.set_access_token('918405736127979521-C5JxvHj24tYgKcB6CFTBdrHCO80JMd4', 'PbMvexD7WXrKMMU7cI1U8605ynlCurcBUnam5hBzvX3Yg')
+auth = tweepy.OAuthHandler(XXXX, XXXX)
+auth.set_access_token(XXXX, XXXX)
 api = tweepy.API(auth)
 
 def send_tweet(text):
