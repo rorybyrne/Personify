108fbc62f5bc684cdccdb3b0426dc1f150a4f6b5
Rory Byrne <roryofbyrne@gmail.com>
2017-10-18 14:50:36 +0000

    [Code - Gen] utils.py changed to preprocessor.py. Functionality is now in a class, and should be robust enough to handle arbitrary n-grams

--- src/ronald_brain/generator.py
+++ src/ronald_brain/generator.py
@@ -2,18 +2,19 @@ import random
 import sys
 import ronald_brain.constants as constants
 
-from ronald_brain import utils
+from ronald_brain.preprocessor import Ngram
+from ronald_brain import preprocessor
 
 
 class Generator:
     """
     This class will hold all of our models (there's a separate model
-     per 2,3,n-gram of words, sentence length, and POS tag etc.)
-    It contains functions which use those models to generate text.
+        per 2,3,n-gram of words, sentence length, and POS tag etc.).
+
     All models are created when the class is initialized with a filename.
-    The entrypoint to get the next word is currently predict_next()
+    The entrypoint to get the next word is predict_next()
     """
-    def __init__(self, sourcefile, word_ngrams=2):
+    def __init__(self, sourcefile, word_ngrams=2, sent_length_ngrams=2):
         """
         Here we initialize the Generator by creating all the models necessary
 
@@ -21,39 +22,38 @@ class Generator:
         :param word_ngrams:
         """
         self.sourcefile = sourcefile
+        self.preprocessor = preprocessor.Preprocessor(sourcefile)
         self.word_n = word_ngrams
+        self.sentence_n = sent_length_ngrams
+
         # we store a unique n-gram model for each n=2 -> n
-        self.word_ngram_models = self.build_word_models(self.word_n) # TODO: Add unigram building into build_word_models()
+        print("Building word n-gram models...")
+        self.word_ngram_models = self.preprocessor.build_ngram_model_list(self.word_n, Ngram.WORD)
+        print("Word n-gram models built!\n")
 
-    def build_word_models(self, n):
-        """
-        Wecursively build a list of models for n > 2.
-        We want a list of models so that we can implement the Backoff algorithm
-        https://www.quora.com/What-is-backoff-in-NLP
-
-        :param models:
-        :return a list of n-gram models:
-        """
-        if(n < 1):
-            # TODO: Exception
-            sys.exit(0)
-
-        if n == 1:
-            return [utils.unigram_probs(self.sourcefile)]
-        else:
-            m = utils.get_n_gram_probs(self.sourcefile, n)
-            return self.build_word_models(n-1) + [m]
+        print("Building sentence-length n-gram models...")
+        self.sentence_ngram_models = self.preprocessor.build_ngram_model_list(self.sentence_n, Ngram.SENT_LENGTH)
+        print("Sentence-length n-gram models built!\n")
 
     def predict_next(self, ngram):
-        # split the ngram by ',' to get the # of words.
-        n = len(ngram.split(constants.WORD_KEY_DELIMITER))
+        """
+        Given an ngram, it consults the models to choose the next word.
+        This is where the logic for using the models will exist. Right now it's a weighted random choice
+            based on word n-grams.
+        TODO: Extend this to include some logic using sentence lengths
+
+        :param ngram:
+        :return:
+        """
         print("Ngram as key: " + ngram)
-        if n == 0:
-            # TODO: Exception
-            sys.exit(0)
-        if n == 1:
-            # if we are given a unigram, use unigram model
-            return self._weighted_choice(self.word_ngram_models[0])
+        # if we are given a unigram, use unigram model
+        if ngram == '': # use unigram model
+            model = self.word_ngram_models[0]
+            foo = self._weighted_choice(model)
+            print(foo)
+            return foo
+
+        n = len(ngram.split(constants.KEY_DELIMITER))
 
         # otherwise get the appropriate ngram model
         current_model = self.word_ngram_models[n]
@@ -62,10 +62,10 @@ class Generator:
             choice = self._weighted_choice(current_model[ngram])
             return choice
         else:
-            backoff_words = ngram.split(',')[1:]
+            backoff_words = ngram.split(' ')[1:]
             print("BackingOff...from " + str(n) + " to " + str(len(backoff_words)))
-            print("Backoff words: " + ', '.join(backoff_words))
-            backoff_ngram = ','.join(backoff_words)
+            print("Backoff words: " + ' '.join(backoff_words))
+            backoff_ngram = ' '.join(backoff_words)
             return self.predict_next(backoff_ngram)
 
 
@@ -89,14 +89,14 @@ class Generator:
                 return k
 
     def generate(self, num_words):
-        # "i" and "am" are basically the seed words, to begin the sentence with.
+        # "we" and "are" are basically the seed words, to begin the sentence with.
         # will need to figure out a way for the system to choose this word intelligently
         # https://github.com/RoryOfByrne/ronald-trump/issues/4
-        output = ["we", "are", "going", "to"]
+        output = ["we", "are"] #, "going", "to"]
         for x in range(num_words):
             leng = len(output)
             # keys in the probability distribution are comma-separated: "first_word,second_word"
-            prev = constants.WORD_KEY_DELIMITER.join(output[leng - self.word_n + 1:])
+            prev = constants.KEY_DELIMITER.join(output[leng - self.word_n + 1:])
             print("prev: " + prev)
             next_word = self.predict_next(prev)
             print("next word: " + next_word)
