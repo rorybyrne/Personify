0141541f0113328f1f0e55758de5c51f38ba4911
Rory Byrne <roryofbyrne@gmail.com>
2017-10-28 21:20:51 +0000

    [Code] The generator now chooses starting words. It's a little hacky but it works

--- src/ronald_mouth/bot.py
+++ src/ronald_mouth/bot.py
@@ -1,9 +1,8 @@
 import tweepy
-from ronald_brain import generator
-#import twitter_credentials
+from conf import twitter_credentials
 
-auth = tweepy.OAuthHandler(XXXX, XXXX)
-auth.set_access_token(XXXX, XXXX)
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_KEY, twitter_credentials.ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def send_tweet(text):
