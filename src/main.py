6f8b9816857fe6ea818f4fe79847db6618073bf6
Rory Byrne <roryofbyrne@gmail.com>
2017-10-31 18:01:49 +0000

    [Code] Made path relative

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from ronald_brain import generator
 from ronald_mouth import bot
 from sys import exit
 
-filename = "/home/rory/projects/ronald/data/trump-tweets.csv"
+filename = "data/trump-tweets.csv"
 gen = generator.Generator(filename, 3, 2)
 
 tweet = gen.generate(140)
