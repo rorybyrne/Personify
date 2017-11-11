b045489173c97f2d081adbb5774fc30f73c7cf0a
Rory Byrne <roryofbyrne@gmail.com>
2017-11-11 19:55:54 +0000

    [Code] Change to main for use on the server

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from ronald_brain import generator
 from ronald_mouth import bot
 from sys import exit
 
-filename = "data/trump-tweets.csv"
-gen = generator.Generator(filename, 3, 2)
+user = 'realdonaldtrump'
+gen = generator.Generator(user, 3, 2)
 
 tweet = gen.generate(140)
-bot.send_tweet(tweet)
+bot.send_tweet(tweet, user)
